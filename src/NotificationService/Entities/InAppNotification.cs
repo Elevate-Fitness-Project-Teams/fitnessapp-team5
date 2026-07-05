0bbb35d554bcfecab3ea579c2af01db52e283979
Muhammad Asaad <muhammad.as3d@gmail.com>
2026-07-06 00:57:08 +0300

    add NotificationService

---
+++ src/NotificationService/Entities/InAppNotification.cs
@@ -0,0 +1,33 @@
+﻿using NotificationService.Entities.Enum;
+
+namespace NotificationService.Entities;
+
+public class InAppNotification
+{
+    public int Id { get; set; }
+    public Guid UserId { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Message { get; set; } = string.Empty;
+    public NotificationType Type { get; set; }
+    public bool IsRead { get; set; }
+    public DateTime SentAt { get; set; }
+
+    private InAppNotification() { }
+
+    public static InAppNotification Create(Guid userId, string title, string message, NotificationType type) =>
+        new()
+        {
+            UserId = userId,
+            Title = title,
+            Message = message,
+            Type = type,
+            IsRead = false,
+            SentAt = DateTime.UtcNow
+        };
+
+    public void MarkAsRead()
+    {
+        if (!IsRead)
+            IsRead = true;
+    }
+}
