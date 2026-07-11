96ce7b790ad10fd952ccf5bbf328837d2973f713
Muhammad Asaad <muhammad.as3d@gmail.com>
2026-07-11 20:40:00 +0300

    ProgressTrackingService entities

---
+++ src/ProgressTrackingService/Entities/Achievement.cs
@@ -0,0 +1,9 @@
+﻿namespace ProgressTrackingService.Entities;
+
+public class Achievement
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public string IconUrl { get; set; } = null!;
+}
---
+++ src/ProgressTrackingService/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProgressTrackingService.Entities;
+
+namespace ProgressTrackingService.Persistence;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<WorkoutLog> WorkoutLogs => Set<WorkoutLog>();
+    public DbSet<WorkoutLogExercise> WorkoutLogExercises => Set<WorkoutLogExercise>();
+    public DbSet<WeightHistory> WeightHistories => Set<WeightHistory>();
+    public DbSet<BodyMeasurement> BodyMeasurements => Set<BodyMeasurement>();
+    public DbSet<Achievement> Achievements => Set<Achievement>();
+    public DbSet<UserAchievement> UserAchievements => Set<UserAchievement>();
+    public DbSet<Streak> Streaks => Set<Streak>();
+    public DbSet<UserStatistic> UserStatistics => Set<UserStatistic>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
